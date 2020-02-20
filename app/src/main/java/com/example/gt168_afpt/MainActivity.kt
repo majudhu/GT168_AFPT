231890aff5fb2c18dcd993a4260007ac73d80b4c
majudhu <majudhu.sup@gmail.com>
2020-02-21 00:53:47 +0500

    read json data

--- app/src/main/java/com/example/gt168_afpt/MainActivity.kt
+++ app/src/main/java/com/example/gt168_afpt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import fi.iki.elonen.NanoHTTPD
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +15,27 @@ class MainActivity : AppCompatActivity() {
 
         object : NanoHTTPD(5000) {
             override fun serve(session: IHTTPSession): Response {
-                return newFixedLengthResponse(
-                    Response.Status.OK,
-                    "application/json",
-                    Gson().toJson(mapOf("hello" to "world"))
-                )
+                val files = HashMap<String, String>()
+                val method = session.method
+                if (method == Method.POST) {
+                    try {
+                        session.parseBody(files)
+                    } catch (ioe: IOException) {
+                        return newFixedLengthResponse(
+                            Response.Status.INTERNAL_ERROR,
+                            MIME_PLAINTEXT,
+                            "SERVER INTERNAL ERROR: IOException: $ioe"
+                        )
+                    } catch (re: ResponseException) {
+                        return newFixedLengthResponse(re.status, MIME_PLAINTEXT, re.message)
+                    }
+
+                }
+                // get the POST body
+//                val postBody = session.queryParameterString
+                // or you can access the POST request's parameters
+//                val postParameter = session.parms["parameter"]
+                return newFixedLengthResponse("OK") // Or postParameter.
             }
         }.start()
     }
