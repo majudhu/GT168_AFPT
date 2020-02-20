8b35d447788a383846aa2a6e987b53d1e486da7b
Majudhu <majudhu.sup@gmail.com>
2020-02-20 16:22:16 +0500

    return basic json output

--- app/src/main/java/com/example/gt168_afpt/MainActivity.kt
+++ app/src/main/java/com/example/gt168_afpt/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.gt168_afpt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import fi.iki.elonen.NanoHTTPD
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : NanoHTTPD(5000) {
+            override fun serve(session: IHTTPSession): Response {
+                return newFixedLengthResponse(
+                    Response.Status.OK,
+                    "application/json",
+                    Gson().toJson(mapOf("hello" to "world"))
+                )
+            }
+        }.start()
     }
 }
