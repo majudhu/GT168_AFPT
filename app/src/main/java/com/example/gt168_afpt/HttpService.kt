0b523e8d0f59d33800021bf3f04354da1bd3422a
majudhu <majudhu.sup@gmail.com>
2020-02-23 00:10:11 +0500

    start http server on device boot

---
+++ app/src/main/java/com/example/gt168_afpt/HttpService.kt
@@ -0,0 +1,85 @@
+package com.example.gt168_afpt
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import android.content.BroadcastReceiver
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
+import fi.iki.elonen.NanoHTTPD
+
+const val HTTP_SERVER_PORT = 5000;
+const val NOTIFICATION_CHANNEL_ID = "HTTP Server Service"
+
+class HttpService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID, "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, 0
+        )
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Http Server")
+            .setContentText("starting server on $HTTP_SERVER_PORT")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1, notification)
+        HttpServer().start();
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+}
+
+class StartOnBoot : BroadcastReceiver() {
+    override fun onReceive(context: Context, arg1: Intent) {
+        val intent = Intent(context, HttpService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent)
+        } else {
+            context.startService(intent)
+        }
+    }
+}
+
+class HttpServer : NanoHTTPD(HTTP_SERVER_PORT) {
+    private val badRequestResponse: String = Gson().toJson(
+        mapOf(
+            "error" to Response.Status.BAD_REQUEST.requestStatus,
+            "message" to Response.Status.BAD_REQUEST.description
+        )
+    )
+
+    override fun serve(session: IHTTPSession): Response {2
+        try {
+            val files = HashMap<String, String>()
+            session.parseBody(files)
+            val json: Map<String, Any> = Gson().fromJson(
+                files["postData"],
+                object : TypeToken<Map<String, Any>>() {}.type
+            )
+            return newFixedLengthResponse(Gson().toJson(json))
+        } catch (e: JsonSyntaxException) {
+            return newFixedLengthResponse(badRequestResponse)
+        } catch (e: ResponseException) {
+            return newFixedLengthResponse(badRequestResponse)
+        }
+    }
+}
--- app/src/main/java/com/example/gt168_afpt/MainActivity.kt
+++ app/src/main/java/com/example/gt168_afpt/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.gt168_afpt
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
-import fi.iki.elonen.NanoHTTPD
-import com.google.gson.reflect.TypeToken
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,27 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        object : NanoHTTPD(5000) {
-            override fun serve(session: IHTTPSession): Response {
-                try {
-                    val files = HashMap<String, String>()
-                    session.parseBody(files)
-                    val json: Map<String, Any> = Gson().fromJson(
-                        files["postData"],
-                        object : TypeToken<Map<String, Any>>() {}.type
-                    )
-                    return newFixedLengthResponse(Gson().toJson(json))
-                } catch (e: JsonSyntaxException) {
-                    return newFixedLengthResponse(
-                        Gson().toJson(
-                            mapOf(
-                                "error" to Response.Status.BAD_REQUEST.requestStatus,
-                                "message" to Response.Status.BAD_REQUEST.description
-                            )
-                        )
-                    )
-                }
-            }
-        }.start()
+        val startIntent = Intent(this, HttpService::class.java)
+        ContextCompat.startForegroundService(this, startIntent);
     }
 }
